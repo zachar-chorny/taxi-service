2bd8b43f216db0d122f9ef207b2c06a67fca4687
Zachar Chorny <ufumoveldar@gmail.com>
2021-12-14 20:31:48 +0200

    taxi service was implemented

---
+++ src/main/java/mate/controller/authentication/LoginController.java
@@ -0,0 +1,47 @@
+package mate.controller.authentication;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import mate.exception.AuthenticationException;
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.AuthenticationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoginController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
+    private static final Logger logger = LogManager.getLogger(LoginController.class);
+    private static final Injector injector = Injector.getInstance("mate");
+    private final AuthenticationService authService = (AuthenticationService) injector
+            .getInstance(AuthenticationService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/authentication/login.jsp")
+                .forward(req,resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        try {
+            Driver driver = authService.login(login, password);
+            logger.info("Authorization was successful. Login: " + login);
+            HttpSession session = req.getSession();
+            session.setAttribute(DRIVER_ID, driver.getId());
+            resp.sendRedirect("/");
+        } catch (AuthenticationException e) {
+            logger.error("Authorization was unsuccessful. Login: " + login);
+            req.setAttribute("errorMsg", e.getMessage());
+            req.getRequestDispatcher("/WEB-INF/views/authentication/login.jsp").forward(req,resp);
+        }
+    }
+}
